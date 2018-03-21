b94e3ff105e48d9469970b3b731b3a32bd754102
woojunchoi <tim.woojune1@gmail.com>
2018-03-21 03:50:14 -0700

    nested graphql query completed

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -3,7 +3,8 @@ const graphql = require('graphql')
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLSchema
+    GraphQLSchema,
+    GraphQLInt
 } = graphql
 
 // dummy data
@@ -13,12 +14,37 @@ var books = [
     { name: 'The Long Earth', genre: 'Sci-Fi', id: '3' },
 ];
 
+var authors = [
+    { name: 'Patrick Rothfuss', age: 44, id: '1' },
+    { name: 'Brandon Sanderson', age: 42, id: '2' },
+    { name: 'Terry Pratchett', age: 66, id: '3' }
+];
+
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        age: {type: GraphQLInt}
+    })
+})
+
 const BookType = new GraphQLObjectType({
    name: 'Book',
    fields: () =>({
        id: {type: GraphQLString},
        name: {type: GraphQLString},
-       genre : {type: GraphQLString}
+       genre : {type: GraphQLString},
+       author: {
+           type:AuthorType,
+           resolve(parentValue,args) {
+               for(let i=0; i<authors.length; i++) {
+                   if(authors[i].id === parentValue.id) {
+                       return authors[i]
+                   }
+               }
+           }
+       }
    })
 })
 
@@ -34,6 +60,19 @@ const RootQuery = new GraphQLObjectType({
                 for(let i=0; i<books.length; i++) {
                     if(books[i].id === args.id) {
                         return books[i]
+                    }
+                }
+            }
+        },
+        author: {
+            type: AuthorType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parentValue,args) {
+                for(let i=0; i<authors.length; i++) {
+                    if(authors[i].id === args.id) {
+                        return authors[i]
                     }
                 }
             }
