829cd33d9db9f1db950d8c24bfcba707c25e4d0c
woojunchoi <tim.woojune1@gmail.com>
2018-03-21 03:20:40 -0700

    booktype and rootquery have been created

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,1 +1,35 @@
 const graphql = require('graphql')
+
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema
+} = graphql
+
+const BookType = new GraphQLObjectType({
+   name: 'Book',
+   fields: () =>({
+       id: {type: GraphQLString},
+       name: {type: GraphQLString},
+       genre : {type: GraphQLString}
+   })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name:'RootQuery',
+    fields:() => ({
+        book: {
+            type: BookType,
+            args:{
+                id:{type:GraphQLString}
+            },
+            resolve(parentValue, args) {
+                args.id
+            }
+        }
+    })
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
