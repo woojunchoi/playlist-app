90e60024fc4afc8f9e45bdec46ca3e08f8d4290c
woojunchoi <tim.woojune1@gmail.com>
2018-03-21 17:29:17 -0700

    react app added

---
+++ server/client/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+        <div id="main">
+          <h1>Woojun's Reading List</h1>
+      </div>
+    );
+  }
+}
+export default App;
