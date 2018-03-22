2041a10af855594232a9c32343af36e87bd7b0af
woojunchoi <tim.woojune1@gmail.com>
2018-03-21 18:38:45 -0700

    now front end and back end are connected and created dom with data from mongodb

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import {ApolloProvider} from 'react-apollo'
 
 // components
 import BookList from './components/BookList';
+import AddBook from './components/AddBook';
+
 
 //apollo client setup
 const client = new ApolloClient({
@@ -17,6 +19,7 @@ class App extends Component {
         <div id="main">
           <h1>Woojun's Reading List</h1>
           <BookList />
+          <AddBook />
       </div>
       </ApolloProvider>
     );
--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -2,24 +2,37 @@ import React, { Component } from 'react';
 import { graphql } from 'react-apollo'
 import { gql } from 'apollo-boost'
 
-const getBooksQuery = gql`{
+const getBooksQuery = gql`
     {
         books {
             name
             id
         }
     }
-}`
+`
 class BookList extends Component {
+    
+    displayBooks(){
+        let data = this.props.data;
+        if(data.loading){
+            return( <div>Loading books...</div> );
+        } else {
+            return data.books.map(book => {
+                return(
+                    <li key={ book.id }>{ book.name }</li>
+                );
+            })
+        }
+    }
     render(){
+        console.log(this.props);
         return(
             <div>
                 <ul id="book-list">
-                    <li>Book name</li>
+                    { this.displayBooks() }
                 </ul>
             </div>
         );
     }
 }
-
 export default graphql(getBooksQuery)(BookList);
